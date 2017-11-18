c0d060b89bfc10240a24352b6a218ac62bd10e57
shuwei <swzhang296@gmail.com>
2017-11-18 18:44:12 +0100

    add lamport algo

--- node.py
+++ node.py
@@ -4,7 +4,8 @@ import uuid
 import sys
 import time
 import json
-from multiprocessing import Process, Pipe, Value, Array, Lock
+import heapq 
+from multiprocessing import Process, Manager, Value, Lock
 
 
 class Node(object):
@@ -14,10 +15,10 @@ class Node(object):
 		# declare a queue to receive meg from other exchange
 		self.queue_name = "queue_" + str(self.id)
 		self.corr_id = str(uuid.uuid4())
-		self.timestamp = 0
-		self.waitting_q = []
 
 	def start(self, exchanges):
+		#set up initial share value
+		self.setup_share_val()
 		#set up connection
 		self.setup_connection() 
 		#set up queues
@@ -26,7 +27,13 @@ class Node(object):
 		self.setup_exchange(exchanges)
 		#set up callback queue
 		self.setup_callbackQueue()
-
+	
+	def setup_share_val(self):
+		# define a lock
+		self.lock = Lock()
+		self.timestamp = Value('i', 0)
+		self.waiting_q = Manager().list()
+	
 	def setup_connection(self):
 		self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 		self.channel = self.connection.channel()
@@ -58,45 +65,75 @@ class Node(object):
 		if self.corr_id == props.correlation_id:
 			self.response = body
 			ack_content = json.loads(body)
-			self.timestamp = max(self.timestamp, ack_content["timestamp"]) + 1
+			print("!!!!!!!!", ack_content)
+			self.lock.acquire() # lock it
+			self.timestamp.value = max(self.timestamp.value, ack_content["timestamp"]) + 1
+			self.lock.release() # release it
+			heapq.heapify(list(self.waiting_q))
+			if(self.waiting_q[0]['id'] == self.id):
+				print("yehp! Enter critique exception!")
+				time.sleep(5)
+				self.timestamp.value += 1
+				message = {"timestamp": self.timestamp.value, "id": self.id, "message_type": "release"}
+				self.send_request(message)
 		print("Get ack: %s" % self.response)
-		print("After ack, Now my timestamp is:", self.timestamp)
-
+		print("After ack, Now my timestamp is: %i \n" % self.timestamp.value)
+	
 	def send_request(self, message):
-		message = json.dumps(message)
+		self.lock.acquire() # lock it		
+		self.timestamp.value += 1
+		message["timestamp"] = self.timestamp.value
+		if(message["message_type"] == "request"):
+			self.waiting_q.append(message) # put the request in the waiting queue
+		elif(message["message_type"] == "release"):
+			print("before pop")
+			print(list(self.waiting_q))
+			ele = heapq.heappop(list(self.waiting_q)) #
+			print("after pop is")
+			print(ele)
+			print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>') 
+			print(list(self.waiting_q))
+		self.lock.release() # release it    
+		
+		message_json = json.dumps(message)
 		self.channel.basic_publish(exchange=self.exchange_id,
 			routing_key='',
 			properties=pika.BasicProperties(
 					reply_to = self.callback_queue,
 					correlation_id = self.corr_id,
 					delivery_mode=2,),
-			body=message)
-		self.timestamp += 1     
+			body=message_json)
 		print("Send request: %s" % message)
-		print("After sending, timestamp is ", self.timestamp)
+		print("After sending, timestamp is ", self.timestamp.value)
 
 	def on_receive(self, ch, method, props, body):
 		request_content = json.loads(body)
-		self.timestamp = max(self.timestamp, request_content["timestamp"]) + 1
-
-		response = {"id": self.id, "timestamp": self.timestamp}        
-		self.channel.basic_publish(exchange='',
+		self.lock.acquire() # lock it
+		if(request_content["message_type"] == "request"):
+			self.waiting_q.append(request_content) # put the request in the waiting queue
+			response = {"id": self.id, "timestamp": self.timestamp.value, "message_type": "response"}        
+			self.channel.basic_publish(exchange='',
 						routing_key=props.reply_to,
 						properties=pika.BasicProperties(correlation_id = \
 															props.correlation_id,
 														delivery_mode=2,),# persisitant message
 						body=json.dumps(response)) 
-		ch.basic_ack(delivery_tag = method.delivery_tag)
+			ch.basic_ack(delivery_tag = method.delivery_tag)
+		elif(request_content["message_type"] == "release"):
+			heapq.heappop(list(self.waiting_q))
+		self.timestamp.value = max(self.timestamp.value, request_content["timestamp"]) + 1
+		self.lock.release() # release it
+		
 		
 		print("Get request: ")		
 		print("Timestamp received is:", request_content["timestamp"])
 		print("Id received is:", request_content["id"])
-		print("After receive, Now my timestamp is:", self.timestamp)
+		print("After receive, Now my timestamp is:", self.timestamp.value)
 
 
 if __name__ == "__main__":
 	if len(sys.argv) != 3:
-        	print("usage: python node.py id numberofnodes")	
+        	print("usage: python node.py id numberofnodes")
 	site_id = sys.argv[1]
 	nb_exchange = sys.argv[2]
 	site = Node(site_id, nb_exchange)
@@ -104,16 +141,20 @@ if __name__ == "__main__":
 	p1 = Process(target=site.channel.start_consuming, args=())
 	p1.start()
 	while(True):
-		func = raw_input('Enter a number:\n 1->send request, 2->listen: \n')
-		if func == "":
+		func = raw_input('Enter a number:\n 1->send request, 2->quit: \n')
+		if func == "" or (func != "1" and func != "2"):
+			print("Enter error")
 			continue
+		if(int(func) == 2):
+			print("yes 2")
+			#sys.exit(0)
 		if(int(func) == 1):
-			message = {"id":site.id, "timestamp":site.timestamp}
-			print("site timestamp is ", site.timestamp)
+			message = {"timestamp":site.timestamp.value, "id":site.id, "message_type": "request"}
+			print("site timestamp is ", site.timestamp.value)
 			site.send_request(message)
 		else:
 			continue
-	p1.join()		
+	#p1.join()		
 		
 	
 	
